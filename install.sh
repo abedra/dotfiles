b24c22de8d12fb92d36541e89531c50828b8bc8d
Aaron Bedra <aaron@aaronbedra.com>
2014-08-16 17:06:59 -0500

    Adding install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+cd $(dirname $0)
+export DOTFILES=$(pwd)
+
+function backup {
+    local FILE=$1
+    if [ -L $FILE ]; then
+        rm $FILE
+    elif [ -e $FILE ]; then
+        mv $FILE $FILE.bak
+    fi
+}
+
+function link_with_backup {
+    local FILENAME=$1
+    local SOURCE=$DOTFILES/$FILENAME
+    local TARGET=$HOME/$FILENAME
+    backup $TARGET
+    ln -sf $SOURCE $TARGET
+}
+
+# git
+link_with_backup .gitconfig
+
+# zsh
+link_with_backup .zsh
+link_with_backup .zshrc
