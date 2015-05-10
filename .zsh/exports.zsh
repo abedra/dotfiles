d1b87fc097a9ad1ad99f0cd860bead1c9206df88
Aaron Bedra <aaron@aaronbedra.com>
2015-05-09 22:22:00 -0500

    Add GOPATH/bin to PATH

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,5 +1,5 @@
 export CDPATH=.:~/src:~/src/personal:~/src/opensource:~/src/configs:~/src/groupon:~/Documents
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+
 export TERM=xterm-256color
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export CLICOLOR=1
@@ -7,6 +7,7 @@ export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
 export PAGER=most
 export EDITOR="emacs -nw"
-
 export JAVA_HOME=`/usr/libexec/java_home`
+
 export GOPATH=~/src/golang
+export PATH=/usr/local/bin:/usr/local/sbin:$GOPATH/bin:$PATH
